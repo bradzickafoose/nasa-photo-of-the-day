919f2fd0dae5d214d445eaaca011f574023b772d
Brad <brad@viziworx.com>
2019-10-09 17:57:24 -0400

    Added NASA logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import NASAPhoto from "./components/NasaPhoto";
+import logo from "../assets/nasa-logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className="logo_container">
+        <img className="logo" src={logo} alt="NASA logo" />
+        <h1>Photo of the Day</h1>
+        <NASAPhoto />
+      </div>
     </div>
   );
 }
