05ea9240fd61add4906272fa1037f4ec64dd8d3a
Brad <brad@viziworx.com>
2019-10-10 09:37:10 -0400

    Setting up components

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,3 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default NasaPhoto;
+export default function NasaPhoto(props) {
+    const [photoState, setPhotoState] = useState('');
+    useEffect(() => {
+        axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=inPjaaJyTMnb0cJsR1l50D3EJ8sNVlM6wD7sADgx`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log("the data was not return", error);
+        });
+    }, []);
+
+}
